cb6e7191faf7c8661b808308d90da36a9af2c4d4
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-08 00:02:48 +0400

    added a new line

--- Seminars/Seminar1/Program.cs
+++ Seminars/Seminar1/Program.cs
@@ -1,5 +1,20 @@
-﻿int num;
+﻿/*
+//Задача 1
+int num;
 Console.Write("Input a number: ");
 num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Your number is " + num);
+*/
+
+//Задача 2. Пользователь должен ввести число, а программа напечатать квадрат этого числа.
+
+int num;
+Console.WriteLine("Input a number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int square = num * num;
+
+Console.WriteLine("The square of " + num + " is " + square);
+Console.WriteLine($"The square of {num} is {square}");
+
