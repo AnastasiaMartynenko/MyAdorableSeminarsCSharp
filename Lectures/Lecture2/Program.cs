cb1a4aa355ffe429511716bbf01237bd219e3f9b
Anastasia <anastasiamartynenko94@gmail.com>
2022-08-10 01:06:25 +0300

    added task1

--- Lectures/Lecture2/Program.cs
+++ Lectures/Lecture2/Program.cs
@@ -1,51 +1,25 @@
-﻿/* 
-//Найти максмимально значение
+﻿// Функция - это подпрограмма (в C# называется 'методом')
+// Задача 1. Найти максимальное число.
 int a1 = 15;
 int b1 = 21;
-int c1 = 39;
+int c1 = 69;
 int a2 = 12;
-int b2 = 100;
+int b2 = 23;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
-int max = a1;
-
-if(b1 > max) max = b1;
+int max = a1; // сначала мы ввели переменную max, в которую положили значение первого аргумента
+if(b1 > max) max = b1; //сравниваем превый аргумент с последующий. Если он больше значит, теперь он максимальный на данномы этапе 
 if(c1 > max) max = c1;
 
 if(a2 > max) max = a2;
 if(b2 > max) max = b2;
 if(c2 > max) max = c2;
 
-if(a3 > max) max = a3;
-if(b3 > max) max = b3;
-if(c3 > max) max = c3;
-
-Console.WriteLine(max);
-*/
-
-// Найти максимальное значение при помощи функции 
-
-
-int max1 = Max1 (a1, b1, c1);
-int Max (int arg1, int arg2, int arg3);
-{
-    int result = arg1;
-    if(arg2 > result) result = arg2;
-    if(arg3 > result) result = arg3;
-    return result;
-}
-
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 100;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23;
-int c3 = 33;
+if(a3 > max) max = a3; 
+if(b3 > max) max = b3; 
+if(b3 > max) max = b3; 
 
 Console.WriteLine(max);
