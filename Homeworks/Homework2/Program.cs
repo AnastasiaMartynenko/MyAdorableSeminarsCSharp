ac4a346f7f6bac08ded61e2119287f314d134223
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-11 05:10:19 +0400

    first task -s done

---
+++ Homeworks/Homework2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int MiddleDigit (int number)
+{
+   int dec = number / 10 % 10;
+   return dec;
+}
+int randomNumber = new Random().Next(100,1000);
+int newNumber = MiddleDigit(randomNumber);
+Console.WriteLine($"{newNumber} is a middle digit of {randomNumber}");
