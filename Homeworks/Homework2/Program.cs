08d6ce9dcac097c2f980e4be3cf951dd37cbca13
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-12 02:12:05 +0400

    created a shitty solution

--- Homeworks/Homework2/Program.cs
+++ Homeworks/Homework2/Program.cs
@@ -8,3 +8,29 @@ int MiddleDigit (int number)
 int randomNumber = new Random().Next(100,1000);
 int newNumber = MiddleDigit(randomNumber);
 */
+
+// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void int FindThirdDigit (number)
+{
+   int[] array = {0, array.Length};
+   int n = array.Length;
+   int index = 0;
+   int third_digit = array[2];  
+   {
+      while (index < 2)
+      {
+         Concole.WriteLine("Here is no third digit of this number ");
+         index++;
+      }
+      if (index == 2)
+      {
+         Console.WriteLine("Here is ");
+      }
+   }
+}
+   
+Console.Wrire("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n = FindThirdDigit(n):
+Console.WriteLine()
