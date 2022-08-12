942a9f13a349ce2e0b63a131c97700eb44fbd277
Anastasia Mart <anastasiamartynenko94@gmail.com>
2022-08-12 18:56:06 +0400

    made another version of task number13

--- Homeworks/Homework2/Program.cs
+++ Homeworks/Homework2/Program.cs
@@ -11,8 +11,10 @@ int newNumber = MiddleDigit(randomNumber);
 */
 // Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 /*
-void FindThirdDigit(int[] array)
-{  int n = array.Length;
+int FindThirdDigit(int[]array)
+
+{  
+   array = new Random();
    int index = 0; 
    {
       while (index < 2)
@@ -22,27 +24,31 @@ void FindThirdDigit(int[] array)
       index++;
       if (index == 2)
       {
-         Console.WriteLine($"Here is the third digit of this number");
+         Console.WriteLine($"Here is the third digit of this number ");
       }
    }
 }
 Console.Write("Input a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int new_digit = FindThirdDigit(number);
+int random_number = Convert.ToInt32(Console.ReadLine());
+int new_digit = FindThirdDigit(random_number);
 Console.WriteLine(new_digit);
+
 */
-
-
-int FindNewDigit ([]int number, int third_digit)
+/*
+int FindThirdDigit ([]int array)
 {
-   int digits = number.Lenght;
-   int index = 0;
+  
+  for(int index < 2; index <array.Length; index++)
 
+  
 
 
 
 
 }
+
+ //int index = 0;
+ //  third_number = number[2];
 
 /*
 Console.Write("Input a number: ");
@@ -110,3 +116,51 @@ Console.WriteLine(new_digit);
 
 int third_digit = array[2]; 
 */
+
+/*
+int FindThirdDigit ([]int array)
+{
+  for(int index < 2; index <array.Length; index++)
+*/
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+bool FindWeekend (int day_number)
+{
+   if(day_number >= 6)
+      return true;
+   
+   else
+      return false;
+}
+Console.Write("Input any number of a week day: ");
+int day_number = Convert.ToInt32(Console.ReadLine());
+
+bool FindDay = FindWeekend(day_number);
+Console.WriteLine(FindDay);
+*/
+
+//// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int FindThirdDigit(int[]array)
+
+{  
+   int third_digit = array[2];
+   int random_number = Convert.ToInt32(new Random());
+   int index = 0; 
+   {
+      while (index < 2)
+      {
+         Console.WriteLine("Here is no third digit of this number ");
+      }
+      index++;
+      if (index == 2)
+      {
+         Console.WriteLine($"Here is the third digit of this number ");
+      }
+   }
+}
+Console.Write("Input a number: ");
+int random_number = Convert.ToInt32(Console.ReadLine());
+int new_digit = FindThirdDigit(third_digit);
+Console.WriteLine(new_digit);
+
